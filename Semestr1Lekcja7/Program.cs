16b24ed48026c570f4d4aaed61964d9b1ef04684
Kamil Szczypczyk <kamil.szczypczyk@microsoft.wsei.edu.pl>
2022-11-08 19:55:47 +0100

    Add project files.

---
+++ Semestr1Lekcja7/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semestr1Lekcja7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //użytkownik wpisuje nazwa skróca pierwiastka lub liczba atomowa (np. H     1)
+
+            Console.WriteLine("Podaj skróconą nazwę pierwiastka lub liczbę atomową");
+            string danePierwiastka = Console.ReadLine();
+            
+            if (danePierwiastka == "H" || danePierwiastka == "1")
+                Console.WriteLine("Szukany Pierwiastek to --> Wodór");
+            else if (danePierwiastka == "CO" || danePierwiastka == "27")
+                Console.WriteLine("Szukany Pierwiastek to --> Kobalt");
+            else if (danePierwiastka == "K" || danePierwiastka == "19")
+                Console.WriteLine("Szukany Pierwiastek to --> Potas");
+            else if (danePierwiastka == "AG" || danePierwiastka == "47")
+                Console.WriteLine("Szukany Pierwiastek to --> Srebro");
+            else if (danePierwiastka == "AU" || danePierwiastka == "79")
+                Console.WriteLine("Szukany Pierwiastek to --> Złoto");
+            else if (danePierwiastka == "MG" || danePierwiastka == "12")
+                Console.WriteLine("Szukany Pierwiastek to --> Magnez");
+            else if (danePierwiastka == "HE" || danePierwiastka == "2")
+                Console.WriteLine("Szukany Pierwiastek to --> Hel");
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Sklep_internetowy_ćwiczenie/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sklep_internetowy_ćwiczenie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj w naszym sklepie");
+            Console.WriteLine("Wybierz interesujący Cię produkt");
+
+            float cena1 = 200f;
+            float cena2 = 150f;
+            float cena3 = 30f;
+            const string KOD = "giganci2022";
+            bool znizka = false;
+
+            Console.WriteLine($"1 - Klawiatura Gamingowa Logitech - {cena1}zł");
+            Console.WriteLine($"2 - Mysz Dell - {cena2}zł");
+            Console.WriteLine($"3 - Podkładka po mysz - {cena3}zł");
+
+            int wybór = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj kod promocyjny");
+            string kodUzytkownika = Console.ReadLine();
+            if (kodUzytkownika == KOD)
+                znizka = true;
+
+
+            if(wybór == 1)
+            {
+                if(znizka)
+                    Console.WriteLine($"Do zapłaty {cena1 * 0.85}zł");
+                else
+                    Console.WriteLine($"Do zapłaty {cena1}zł");
+            }
+            else if (wybór == 2)
+            {
+                if(znizka)
+                    Console.WriteLine($"Do zapłaty {cena2 * 0.85}zł");
+                else
+                    Console.WriteLine($"Do zapłaty {cena2}zł");
+            }
+            else if (wybór == 3)
+            {
+                if(znizka)
+                    Console.WriteLine($"Do zapłaty {cena3 * 0.85}zł");
+                else
+                    Console.WriteLine($"Do zapłaty {cena3}zł");
+            }   
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
